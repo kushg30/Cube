447547a295e98e5123a7700398efb3b4c3e47157
kushg30 <48609075+kushg30@users.noreply.github.com>
2019-10-20 14:12:30 +0530

    Add files via upload

---
+++ Scripts/movement.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+
+
+
+
+public class movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    public float forwardforce = 1000f;
+    public float sidewaysforce = 500f;
+   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0, forwardforce * Time.deltaTime);
+
+        if(  Input.GetKey("d")  )
+        {
+            rb.AddForce(sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+         if( Input.GetKey("a") )
+        {
+            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0 , ForceMode.VelocityChange);
+
+        }
+
+
+         if(rb.position.y < -3f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
---
+++ Scripts/playcollision.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class playcollision : MonoBehaviour
+{
+    public movement move;
+    
+
+      void OnCollisionEnter(Collision collisionInfo)
+
+    {   
+        if(collisionInfo.collider.tag=="Obstacle")
+        {
+            move.enabled = false;
+
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+
+        
+    }
+
+
+
+
+
+}
